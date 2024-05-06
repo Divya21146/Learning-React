48fa8671e14f5fab2238cda2e5f5ef3b244b3526
Divi21146 <divi21146@gmail.com>
2024-05-06 13:08:03 +0530

    displaying products based on input

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 
 const CardList = () => {
   const [products, setProducts] = useState([]);
+  const [numProducts, setNumProducts] = useState(0);
+  const [overallCount, setOverallCount] = useState(0);
 
   useEffect(() => {
     fetch('https://dummyjson.com/products')
@@ -11,13 +13,17 @@ const CardList = () => {
         }
         return response.json();
       })
-      .then(data => setProducts(data.products))
+      .then(data => {
+        setProducts(data.products);
+        setNumProducts(data.products.length);
+        setOverallCount(data.products.length);
+      })
       .catch(error => console.error('Error fetching products:', error));
   }, []);
 
   const renderCards = () => {
     const cards = [];
-    for (let i = 0; i < 5 && i < products.length; i++) {
+    for (let i = 0; i < numProducts && i < products.length; i++) {
       const product = products[i];
       cards.push(
         <div key={product.id} className="card">
@@ -31,10 +37,20 @@ const CardList = () => {
     return cards;
   };
 
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    if (!isNaN(value) && value !== '') { 
+      setNumProducts(parseInt(value));
+    } else {
+      setNumProducts(parseInt(overallCount));
+    }
+  };
+
   return (
     <div className="card-list">
-      {renderCards()}
-    </div>
+    <input type="text" id="inputField" name="inputField" onChange={handleInputChange} />
+    {renderCards()}
+  </div>
   );
 };
 
